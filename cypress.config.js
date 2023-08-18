3cfcc31330730d24cfb23404e3dc5d6d693e5115
ChenniDJack <chenniveera.s@gmail.com>
2023-08-18 11:26:07 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 5000,
-    specPattern: "**/*.js",
+    // specPattern: "**/*.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
