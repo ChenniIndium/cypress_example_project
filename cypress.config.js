e2d084e5967d4db66df019e9044e43d4e5722a60
ChenniDJack <chenniveera.s@gmail.com>
2023-08-18 11:46:10 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  integration: {
     baseUrl:'https://example.cypress.io',
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 5000,
-    // specPattern: "**/*.js",
+    // integrationFolder: "cypress/integration",
+    // testFiles: "**/*.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
