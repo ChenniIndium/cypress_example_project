932feded87eba37bf23499816495457c5d3fedd8
ChenniDJack <chenniveera.s@gmail.com>
2023-08-18 11:09:04 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ module.exports = defineConfig({
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 5000,
-    integrationFolder: "cypress/integration",
-    testFiles: "**/*.js",
+    // integrationFolder: "cypress/integration",
+    // testFiles: "**/*.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
